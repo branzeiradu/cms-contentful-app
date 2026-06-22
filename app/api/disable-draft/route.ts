9aa05a63ff81fc6c94ac679b458f04516c58f1ce
Radu Branzei <radub@ensemble.com>
2026-06-22 16:16:10 +0300

    Fix draft mode for Next.js 14

--- app/api/disable-draft/route.ts
+++ app/api/disable-draft/route.ts
@@ -1,6 +1,8 @@
 import { draftMode } from "next/headers";
 
 export async function GET(request: Request) {
-  draftMode().disable();
+  const draft = await draftMode();
+  draft.disable();
+
   return new Response("Draft mode is disabled");
 }
