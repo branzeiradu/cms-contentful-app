750b48a8a526a9b8f154005332d994e01bd3caa6
Radu Branzei <radub@ensemble.com>
2026-06-23 18:30:55 +0300

    Revalidate log

--- app/api/revalidate/route.ts
+++ app/api/revalidate/route.ts
@@ -2,6 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 import { revalidateTag } from "next/cache";
 
 export async function POST(request: NextRequest) {
+  console.log("REVALIDATE ROUTE HIT");
   const requestHeaders = new Headers(request.headers);
   const secret = requestHeaders.get("x-vercel-reval-key");
   console.log(`request header x-vercel-reval-key: ${secret}`);
